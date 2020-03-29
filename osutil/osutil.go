6814c43dbc32524711e5608e15e8a65babe7559c
Clint J Edwards <clint.j.edwards@gmail.com>
2020-03-29 17:52:13 -0400

    remove errors as they are mostly likely to be project specific

--- osutil/osutil.go
+++ osutil/osutil.go
@@ -7,6 +7,7 @@ import (
 )
 
 // ExecuteBashCmd takes in a command string and executes it using bash
+// returns both stdout and stderr to user as a combined blob
 func ExecuteBashCmd(command string, env []string, workingDir string) ([]byte, error) {
 	// Create context with timeout
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
@@ -17,6 +18,6 @@ func ExecuteBashCmd(command string, env []string, workingDir string) ([]byte, er
 	cmd.Env = env
 	cmd.Dir = workingDir
 
-	// Execute command
+	// Execute command and return combined output to user
 	return cmd.CombinedOutput()
 }
